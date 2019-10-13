54a86608f202f7ae0542802184c1f29f0a9b807f
Xmo <37503114+darkXmo@users.noreply.github.com>
2019-10-13 23:16:26 +0800

    X

--- MongoDBTest/Test.py
+++ MongoDBTest/Test.py
@@ -1,10 +1,43 @@
 import pymongo
+import random
 mongo_client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+import time
 
 db = mongo_client['test']
-print(db.collection_names())
-countries = db['countries']
-print(countries.find())
-results = countries.find()
-for result in results:
-    print(result)
+
+def timedTask(db):
+    while(True):
+        types = db['types']
+        types.delete_many({})
+        type1 = {
+            'distance' : round(random.random(),3)
+        }
+        type2 = {
+            'distance' : round(random.random(),3)
+        }
+        type3 = {
+            'distance' : round(random.random(),3)
+        }
+        type4 = {
+            'distance' : round(random.random(),3)
+        }
+        type5 = {
+            'distance' : round(random.random(),3)
+        }
+        types.insert_many([type1, type2, type3, type4, type5])
+
+
+
+        countries = db['countries']
+        countries.delete_many({})
+        country1 = {
+            'distance' : round(random.random(),3)
+        }
+        country2 = {
+            'distance' : round(random.random(),3)
+        }
+        countries.insert_many([country1, country2])
+
+        time.sleep(3)
+
+timedTask(db);
--- pyecharts-flask-demo/app.py
+++ pyecharts-flask-demo/app.py
@@ -11,13 +11,15 @@ app = Flask(__name__, static_folder="templates")
 mongo_client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
 db = mongo_client['test']
 countries = db['countries']
-results = countries.find()
-print(results)
-dis = []
-for result in results:
-    dis.append(result['distance'])
+types = db['types']
+
+
 
 def country_base() -> Bar:
+    results0 = countries.find()
+    dis0 = []
+    for result in results0:
+        dis0.append(result['distance'])
     c = (
         Bar(
             init_opts=opts.InitOpts(
@@ -29,10 +31,33 @@ def country_base() -> Bar:
             )
         )
         .add_xaxis(["中国", "美国"])
-        .add_yaxis("评分差距", dis)
+        .add_yaxis("评分差距", dis0)
         .set_global_opts(title_opts=opts.TitleOpts(title="国家"))
     )
     return c
+
+def type_base() -> Bar:
+    results1 = types.find()
+    dis1 = []
+    for result in results1:
+        dis1.append(result['distance'])
+    c = (
+        Bar(
+            init_opts=opts.InitOpts(
+                animation_opts=opts.AnimationOpts(
+                    animation_delay=500, 
+                    animation_easing="elasticOut",  # 延时动画效果
+                    animation_duration_update=1000
+                ),
+            )
+        )
+        .add_xaxis(["类型1", "类型2", "类型3", "类型4", "类型5"])
+        .add_yaxis("评分差距", dis1)
+        .set_global_opts(title_opts=opts.TitleOpts(title="国家"))
+    )
+    return c
+
+
 
 
 @app.route("/")
@@ -41,11 +66,14 @@ def index():
 
 
 @app.route("/countryChart")
-def get_bar_chart():
+def get_countryBar_chart():
     c = country_base()
     return c.dump_options_with_quotes()
 
-
+@app.route("/typeChart")
+def get_typeBar_chart():
+    c = type_base()
+    return c.dump_options_with_quotes()
 
 
 if __name__ == "__main__":
